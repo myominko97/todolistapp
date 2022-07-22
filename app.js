95da9bd2ae6d39649c8ca6eced31f087dd8817dd
myominko97 <myominko.m2kdev@gmail.com>
2022-07-23 00:19:58 +0630

    finish todolist error

--- app.js
+++ app.js
@@ -50,8 +50,9 @@ function showTodo(filter){
     // if li isn't empty, insert this value inside taskbox else insert span
     taskBox.innerHTML = li || `<span> You don't have any task here </span>`;
 }
-}
 showTodo("all");
+
+}
 function showMenu(selectedTask){
     //getting task menu div
     let taskMenu = selectedTask.parentElement.lastElementChild;
